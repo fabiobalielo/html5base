a3e2793b7910685465f8addb6aba15f84051e8e4
fabiobalielo <fabiobalielo@hotmail.com>
2014-01-20 17:16:52 -0200

    html5

---
+++ js/plugins.js
@@ -0,0 +1,6 @@
+
+/*
+	SITE: Cianorte F.C. / Leão do Vale do Ivaí
+	AGENCIA: Pubblicite Comunicação
+	AUTHOR: Fábio Balielo / fabiobalielo@hotmail.com
+*/
---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+/*
+	SITE: Cianorte F.C. / Leão do Vale do Ivaí
+	AGENCIA: Pubblicite Comunicação
+	AUTHOR: Fábio Balielo / fabiobalielo@hotmail.com
+*/
+
+$(document).ready(function() {
+
+});
